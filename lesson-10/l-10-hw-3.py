38b84c51c8fdc091c0df5c5b5831793f10a89fab
Alex <saver2003@gmail.com>
2021-09-17 22:20:12 +0600

    hw 3

--- lesson-10/l-10-hw-3.py
+++ lesson-10/l-10-hw-3.py
@@ -1,27 +1,55 @@
 order = input("Закажите блюдо(шаурма, самса, пирожок): ")
 
-filling = ''
+shawarma_filling = ''
+samsa_filling = ''
+pie_filling = ''
 waiting = ''
+shawarma_quantity = 0
+samsa_quantity = 0
+pie_quantity = 0
+warm_up = ''
 
 if order == "шаурма":
-    filling = input("Выберите начинку(мясо, курица): ")
+    shawarma_filling = input("Выберите начинку (мясо, курица): ")
+    shawarma_quantity = input("Сколько шаурма вы будете? ")
+    warm_up = input("Подогреть? ")
 elif order == "самса":
-    filling = input("Выберите начинку(мясо, курица, сыр): ")
-    if filling == "сыр":
+    samsa_filling = input("Выберите начинку(мясо, курица, сыр): ")
+    if samsa_filling == "сыр":
         print("Самсы с сыром закончилсь, свежие будут готовы через 15 минут")
-        waiting = input("Вы будете ждать(да, нет): ")
+        waiting = input("Вы будете ждать? (да, нет): ")
+        if waiting == "да":
+            samsa_quantity = int(input("Сколько самс вы будете? "))
+            warm_up = input("Подогреть? ")
+    else:
+        warm_up = input("Подогреть? ")
 
 elif order == "пирожок":
-    filling = input("Выберите начинку(картошка, капуста): ")
-
-print(waiting)
-
+    pie_filling = input("Выберите начинку(картошка, капуста): ")
+    pie_quantity = input("Сколько пирожков вы будете? ")
+    warm_up = input("Подогреть? ")
 
 def onExit(waiting):
     if waiting == "нет":
+        print("До свидания")
         return
-    else:
-        print('as;ldkfj')
+
+if warm_up == "да":
+    warm_up = "подогреть"
+else:
+    warm_up = "греть не нужно"
+
+def orderInput(quantity, order, filling, warming):
+    print("Ваш заказ: %s %s с начинкой %s %s"  % (quantity, order, filling, warming))
+
+
 
 
 onExit(waiting)
+
+if order == "шаурма":
+    orderInput(shawarma_quantity, order, shawarma_filling)
+elif order == "самса" and waiting == "да":
+    orderInput(samsa_quantity, order, samsa_filling, warm_up)
+elif order == "пирожок":
+    orderInput(order, pie_filling)
