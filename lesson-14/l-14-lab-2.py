824ae582e5c5ac2e6a22c661d8900acfbf5cf39d
Alex <saver2003@gmail.com>
2021-09-20 21:50:21 +0600

    lesson 14 lab 2

--- lesson-14/l-14-lab-2.py
+++ lesson-14/l-14-lab-2.py
@@ -12,12 +12,30 @@ movies = [
             'Jack': 9,
             'Jane': 10
         }
+    },
+    {
+        'name': 'Forrest Gump',
+        'ratings': {
+            'Nick': 10,
+            'Mary': 10
+        }
     }
 ]
 
 
 def show_movie(movie):
-    print('{:<35}{:<15}'.format(movie['name'], 'rating'))
+    ratings = movie['ratings'].values()
+    rating_sum = 0
+    for i in ratings:
+        rating_sum += i
+    average_rating = rating_sum / len(ratings)
+    print('{:<35}{:<15}'.format(movie['name'], average_rating))
+
+
+def find_movie(search_value):
+    for movie in movies:
+        if movie['name'].lower() == search_value.lower():
+            return movie
 
 
 def command_add():
@@ -28,13 +46,16 @@ def command_add():
 
 
 def command_delete():
-    print('delete')
+    title = input("Введите название фильма > ")
+    movie = find_movie(title)
+    movies.remove(movie)
+    commands['list']()
 
 
 def command_list():
     print('{:<35}{:<15}'.format('Movie', 'Rating'))
-    for i in movies:
-        show_movie(i)
+    for movie in movies:
+        show_movie(movie)
 
 
 def command_rate():
@@ -42,7 +63,8 @@ def command_rate():
 
 
 def command_find():
-    print('find')
+    title = input("Введите название фильма > ")
+    movie = find_movie(title)
 
 
 def command_exit():
